af944f587629e210a2472fd68c06d6e569e57d23
이경용 <sleekydz86@naver.com>
2023-04-19 21:17:53 +0900

    정리
    정리

--- src/main/java/com/example/validation/controller/MainController.java
+++ src/main/java/com/example/validation/controller/MainController.java
@@ -1,10 +1,14 @@
 package com.example.validation.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
-    @RequestMapping
+    @RequestMapping(value={"/index.do","/"})
+    public String index() {
+        return "index";
+    }
 }
