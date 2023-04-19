5f92392bbbc7428a925f58cbad19d132b5e17e09
이경용 <sleekydz86@naver.com>
2023-04-19 21:08:20 +0900

    정리
    정리

---
+++ src/main/java/com/example/validation/controller/MainController.java
@@ -0,0 +1,10 @@
+package com.example.validation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping
+}
---
+++ src/main/java/com/example/validation/domain/UserValidator.java
@@ -0,0 +1,4 @@
+package com.example.validation.domain;
+
+public class UserValidator {
+}
