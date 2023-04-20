17d7b9298b0dab39a2f6cabfeb83b9f5245cf2c9
이경용 <sleekydz86@naver.com>
2023-04-20 13:07:36 +0900

    바인딩 에러처리
    바인딩 에러처리

--- src/main/java/com/example/springvalidation/controller/MainController.java
+++ src/main/java/com/example/springvalidation/controller/MainController.java
@@ -1,15 +1,24 @@
 package com.example.springvalidation.controller;
 
 import com.example.springvalidation.model.UserDTO;
+import com.example.springvalidation.model.UserValidator;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
+
+
+    @Autowired
+    private UserValidator userValidator;
+
     @RequestMapping(value = {"/", "index"}, method = RequestMethod.GET)
     public String enroll(Model model) {
         UserDTO user = new UserDTO();
@@ -17,15 +26,13 @@ public class MainController {
         return "index";
     }
 
-    @RequestMapping(value = "result", method = RequestMethod.POST)
-    public String result(@Valid UserDTO user, BindingResult bindingResult, Model model) {
-        //userValidator.validate(user, bindingResult);
-
-        if(bindingResult.hasErrors()) {
-            model.addAllAttributes(bindingResult.getModel());
+    @PostMapping("/result")
+    public String registerUser(@ModelAttribute("user") UserDTO user, BindingResult bindingResult, Model model) {
+        userValidator.validate(user, bindingResult);
+        if (bindingResult.hasErrors()) {
             return "index";
         } else {
-            model.addAttribute("user",user);
+            model.addAttribute("user", user);
             return "result";
         }
     }
